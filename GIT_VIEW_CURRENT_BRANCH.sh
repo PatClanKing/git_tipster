898b8cb03604bc8b9036fb3e2e49014a0983018c
patrick <p.meenan10@gail.com>
2023-09-27 15:06:03 +0300

    Added first command

---
+++ GIT_VIEW_CURRENT_BRANCH.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the alias and the command it represents
+script_name=$(basename "$0")
+new_alias="${script_name%.*}"
+
+
+# Define the command to check the current Git branch
+command_to_alias="\"git branch | grep '*' | cut -d ' ' -f 2\""
+
+# Add the alias to the user's .bashrc file
+echo "alias $new_alias=$command_to_alias" >> ~/.bashrc
+
+# Reload the user's shell to make the alias immediately available
+source ~/.bashrc
+
+# Display a message indicating the alias has been added
+echo "Alias '$new_alias' for '$command_to_alias' has been added to your system."
